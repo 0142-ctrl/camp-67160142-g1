338bd4f2d9b1ec43ae7594e506e9c9bb5cdaf6c6
0142-ctrl <67160142@go.buu.ac.th>
2025-12-26 16:51:14 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/se', function () {
 });
 
 Route::get('/mycontroller', [App\Http\controllers\MyController::class, 'index']);
+Route::get('/calculate', [App\Http\controllers\MyController::class, 'info']);
+Route::post('/calculate', [App\Http\controllers\MyController::class, 'calculate']);
+
+Route::resource('/flights', [App\Http\controllers\MyController::class, 'class']);
